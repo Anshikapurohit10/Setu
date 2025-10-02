1a073ddd34a1f5b5a0448c33abde6c18efdb252f
Anshikapurohit10 <anshikapurohit4@gmail.com>
2025-10-02 14:42:09 +0530

    Update api.js

--- siksha/src/component/auth/api.js
+++ siksha/src/component/auth/api.js
@@ -1,45 +1,4 @@
-// import axios from "axios";
 
-// const API = axios.create({
-//    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api"
-// });
-//   // baseURL: "http://localhost:5000/api",
-//    // ✅ backend base URL
-// });
-
-// // Automatically attach JWT token to each request
-// API.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("tresetu_token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// export default API;
-
-// import axios from "axios";
-
-// // Create axios instance
-// // const API = axios.create({
-// //   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api", // backend base URL
-// //   withCredentials: true,
-// // });
-// const api = axios.create({
-//   baseURL: "https://setu-h683.onrender.com/api",
-//   withCredentials: true // ✅ important for cookies/auth
-// });
-
-// // Automatically attach JWT token to each request
-// API.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("tresetu_token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// export default API;
 import axios from "axios";
 
 // Create axios instance
